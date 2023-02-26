7489750dcbe7e8b348d7d1a1d3213cc66d8a99e6
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-02-26 22:17:05 +0530

    Added Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div className="App">
       <h1>This is Chirp</h1>
+      <Sidebar /> 
     </div>
   )
 }
 
-export default App
+export default App;
