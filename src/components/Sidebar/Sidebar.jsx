9ed341345edcd6d34b9d7ee640bde8b2aff09f19
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-03-01 23:27:21 +0530

    Added Sidebar Option Component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Sidebar.css";
-import SidebarOption from "./SidebarOption/SidebarOption";
+import SidebarOption from "./SidebarOption";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
         <h1>Sidebar</h1>
-        <TwitterIcon />
+        <SidebarOption text="Twitter" Icon={TwitterIcon}/>
+        <SidebarOption text="Home" Icon={HomeIcon}/>
+        <SidebarOption text="Explore" Icon={ExploreIcon}/> 
+        <SidebarOption text="Notifications" Icon={NotificationsIcon}/>
         </div>
     );
 };
