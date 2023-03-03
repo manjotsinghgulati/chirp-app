44f1ba77bf47d901848f10431d6a67ed22940ec4
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-03-03 22:06:09 +0530

    Added More Sidebar Options

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,6 +5,12 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import HomeIcon from "@mui/icons-material/Home";
 import ExploreIcon from "@mui/icons-material/Explore";
 import NotificationsIcon from "@mui/icons-material/Notifications";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+// import { Button } from "@material-ui/core";
 
 const Sidebar = () => {
     return (
@@ -14,6 +20,11 @@ const Sidebar = () => {
         <SidebarOption text="Home" Icon={HomeIcon}/>
         <SidebarOption text="Explore" Icon={ExploreIcon}/> 
         <SidebarOption text="Notifications" Icon={NotificationsIcon}/>
+        <SidebarOption text="Messages" Icon={MailOutlineIcon}/>
+        <SidebarOption text="Bookmarks" Icon= {BookmarkBorderIcon}/>
+        <SidebarOption text="Lists" Icon={ListAltIcon}/>
+        <SidebarOption text="Profile" Icon={PermIdentityIcon}/>
+        <SidebarOption text="More" Icon={MoreHorizIcon}/>
         </div>
     );
 };
