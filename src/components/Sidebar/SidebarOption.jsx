48a110abaf522163480fdfdcf02cb81ea26330b0
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-03-05 23:32:45 +0530

    Sidebar Color and CSS Changes

--- src/components/Sidebar/SidebarOption.jsx
+++ src/components/Sidebar/SidebarOption.jsx
@@ -5,7 +5,7 @@ const SidebarOption = ({ active, text, Icon }) => {
     return (
         <div className={`sidebarOption ${active && 'sidebarOption--active'}`}>
          <Icon />
-         <h2>{text}</h2>
+         <p>{text}</p>
         </div>
     );
 }
