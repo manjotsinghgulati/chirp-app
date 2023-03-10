61a5dffc8b5c18f8ab04882ee5a5ea42bf7dc931
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-03-10 23:55:35 +0530

    Created Widget Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Sidebar from './components/Sidebar/Sidebar'
 import Feed from './components/Feed/Feed'
+import Widgets from './components/Widgets/Widgets'
 
 const App = () => {
   return (
     <div className="App">
       <Sidebar /> 
       <Feed />
+      <Widgets />
     </div>
   )
 }
