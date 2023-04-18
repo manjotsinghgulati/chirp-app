beccdc4035be3e264f33874603e36ad99ac7ff42
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-04-19 00:16:57 +0530

    Feed and Widgets Changes

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,15 +1,39 @@
 import React from "react";
 import "./Feed.css";
-import TweetBox from "./PostBox";
-import Post from "./Post";
+import PostBox from "../PostBox/PostBox"
+import Post from "../Post/Post"
 
 const Feed = () => {
+    let api = "https://dummyjson.com/posts"
+    const [posts, setPosts] = React.useState([]);
+    const fetchApi = fetch(api)
+    .then((response) => response.json())
+    .then((data) => setPosts(data))
+    .catch((error) => console.log(error));
+
+
     return (
-        <div className="feed">
-            <div className="feed_header">
-                <h1>Feed</h1>
-            </div>
+    <div className="feed">
+        <div className="feed__header">
+            <h2>Home</h2>
         </div>
+
+      <PostBox />
+
+      {/* <FlipMove> */}
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      {/* </FlipMove> */}
+    </div>
     );
 }
 
