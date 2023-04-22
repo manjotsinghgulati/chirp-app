7f0f326bd9a350a6482bba9d93dd9ac2ec38a034
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-04-22 13:09:16 +0530

    Added Posts to Feed Comp

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,40 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
-import PostBox from "../PostBox/PostBox"
-import Post from "../Post/Post"
+import PostBox from "../PostBox/PostBox";
+import Post from "../Post/Post";
 
 const Feed = () => {
-    let api = "https://dummyjson.com/posts"
-    const [posts, setPosts] = React.useState([]);
-    const fetchApi = fetch(api)
-    .then((response) => response.json())
-    .then((data) => setPosts(data))
-    .catch((error) => console.log(error));
+  // const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+    
+  //   let api = "https://dummyjson.com/posts/5";
+  //   fetch(api)
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => console.log(data))
+  //     .catch((error) => console.log(error));
+  // }, []);
 
 
-    return (
+            
+
+  let posts = [
+    {
+      "id": 1,
+      "text": "My first post on this new platform",
+    },
+    {
+      "id": 2,
+      "text": "This is post 2",
+    },
+    {
+      "id": 3,
+      "text": "This is post 3",
+    },
+    {
+      "id": 4,
+      "text": "This is post 4",
+    },
+    {
+      "id": 5,
+      "text": "This is post 5",
+    }
+]
+
+  return (
     <div className="feed">
-        <div className="feed__header">
-            <h2>Home</h2>
-        </div>
-
+      <div className="feed__header">
+        <h1>Home</h1>
+      </div>
       <PostBox />
-
-      {/* <FlipMove> */}
-        {posts.map((post) => (
-          <Post
-            key={post.text}
-            displayName={post.displayName}
-            username={post.username}
-            verified={post.verified}
-            text={post.text}
-            avatar={post.avatar}
-            image={post.image}
-          />
-        ))}
-      {/* </FlipMove> */}
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          displayName="Manjot Singh"
+          username="manjotsingh" 
+          verified={true}
+          text={post.text}
+          image="https://randomwordgenerator.com/img/picture-generator/chair-1840011_640.jpg"
+          avatar="https://pbs.twimg.com/profile_images/1576549820034322432/-C25AT9Z_400x400.jpg"
+        />
+      ))}
     </div>
-    );
-}
+  );
+};
 
 export default Feed;
