d987e2d0141b9483bd3a7efa2d60d169949a6c99
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-04-23 13:31:34 +0530

    Added Explore Component

---
+++ src/components/Explore/Explore.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./Explore.css";
+import Post from "../Post/Post";
+import { db } from "../../firebase"
+import { collection, onSnapshot, query, orderBy, limit } from "firebase/firestore";
+import FlipMove from "react-flip-move";
+
+const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = () => {
+    const q = query(collection(db, "posts"), orderBy("createdAt" , "desc"));
+    onSnapshot(q, (postData) => {
+      setPosts(postData.docs.map((doc) => doc.data()))
+    })
+  }
+
+  useEffect(() => { 
+    fetchPosts();
+  }, [])
+
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h1>Explore</h1>
+      </div>
+      <FlipMove> 
+        {posts.map((post) => (
+            <Post
+              key={post.text}
+              displayName={post.displayName}
+              username={post.username}
+              verified={post.verified} 
+              text={post.text}
+              avatar={post.avatar}
+              image={post.image}
+            />
+          ))}
+      </FlipMove>
+    </div>
+  );
+};
+
+export default Feed;
